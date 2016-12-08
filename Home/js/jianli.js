3b2cafa40a719c4825f1d0c79f799681735a5a3b
findmoon <1286637198@qq,com>
2016-12-08 15:39:12 +0800

    myblog

--- Home/js/jianli.js
+++ Home/js/jianli.js
@@ -1,7 +1,7 @@
 $(function(){
 			$('#fullpage').fullpage({
 				continuousVertical: true,
-				anchors:['page1','page2','page3','page4'],
+				anchors:['page1','page2','page3','page4','page5'],
 				navigation: true,
 				navigationPosition: 'left',
 				navigationTooltips:['index','works','skills','introduce'],
@@ -38,14 +38,37 @@ $(function(){
 					$('.skills .skilltip').on('click',function(){
 						var $this = $(this),
 							idx = $this.index();
+							// console.log($this.parents('li'))
 						$this.parents('li').siblings().find('.skilltip').removeClass('active');
 						$this.addClass('active');
-						$this.parents('li').siblings().find('.active').fadeOut(1000,function(){
+						$this.parents('li').siblings().find('.content.active').fadeOut(1000,function(){
 							   //console.log(1);
 								$(this).removeClass('active');
 								$this.parents('li').find('.content').addClass('active').fadeIn(1000);
 							});
 					});
+
+					// autoPlay();
+
+					// function autoPlay(){
+					// 	var curInd = 0;
+					// 	// console.log($('.skills').find('li').eq(0));
+					// 	var $posit = $('.skills .skilltip');
+					// 	var leng = $posit.parents('li').length;
+					// 	setInterval(function(){
+					// 		curInd = (curInd + 1)%leng;//获取活动的位置
+					// 		// console.log($posit.parents('li').eq(curInd));
+					// 		// $posit.parents('li').find('.skilltip').removeClass('active');
+							
+					// 		$posit.parents('li').find('.active').fadeOut(1000,function(){
+					// 			   //console.log(1);
+					// 				$(this).fadeIn().removeClass('active');
+					// 				$posit.parents('li').eq(curInd).find('.content ,.skilltip').addClass('active').fadeIn(1000);
+					// 				// $posit.parents('li').eq(curInd).find('.skilltip').addClass('active').fadeIn(1000);//当前活动位置效果
+					// 			});
+
+					// 	},1500)
+					// }
 				}
 			});
 
